5f4f658d73c3f159b8b57fb52cd5700ee4a355ec
Kamil Kwiatkowski <kaczy.kwiatkowski@gmail.com>
2022-05-29 20:34:46 +0200

    finished

--- src/Components/Table.jsx
+++ src/Components/Table.jsx
@@ -1,29 +1,41 @@
 import React from "react"
-import {DataGrid } from "@mui/x-data-grid"
+import { DataGrid } from "@mui/x-data-grid"
+import styled from "styled-components"
+import { insertStyles } from "@emotion/utils"
+
 function Table() {
-    const [items,setItems] = React.useState([])
+  const [items, setItems] = React.useState([])
   React.useEffect(() => {
-      fetch("https://reqres.in/api/products")
-      .then(res => res.json())
-      .then(
-        (result) => {
-            setItems(result.data)
-        })
-          
+    fetch("https://reqres.in/api/products")
+      .then((res) => res.json())
+      .then((result) => {
+        setItems(result.data)
+      })
   }, [])
+  function getFullName(params) {
+    return (params.row.style = `backgroundColor:${params.row.value}`)
+  }
   console.log(items)
   const columns = [
     { field: "id" },
     { field: "name", headerName: "Name", width: 150 },
     { field: "pantone_value", headerName: "Pantone Value", width: 150 },
     { field: "year", headerName: "Year", width: 150 },
-    { field: "color", headerName: "Color", width: 150 },
+    {
+      field: "color",
+      headerName: "Color",
+      width: 150,
+
+      renderCell: (params) => (
+        <div style={{ backgroundColor: `${params.value}`, width:"100%", height:"100%" }}></div>
+      ),
+    },
   ]
-  
+
   return (
     <DataGrid
       rows={items}
-      pageSize={9}
+      pageSize={5}
       columns={columns}
       sx={{ height: "40rem", width: "auto", mr: 5, ml: 5 }}
     />
